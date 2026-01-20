f74a5a2bff6a89be0c76e3e5c50cb6fcef99f29f
samwhelp <samwhelp@users.noreply.github.com>
2026-01-20 23:15:52 +0800

    fix

---
+++ demo/iso-builder/demo-iso-builder-in-docker-start/main/debian-iso-builder-in-docker/helper/mak/debian-iso-builder-respin-xfce.mk
@@ -0,0 +1,5 @@
+
+
+debian-iso-builder-respin-xfce:
+	$(REF_PLAN_DIR_PATH)/master-build.sh "debian-iso-builder-respin-xfce"
+.PHONY: debian-iso-builder-respin-xfce
---
+++ demo/iso-builder/demo-iso-builder-in-docker-start/main/debian-iso-builder-in-docker/helper/mak/help.mk
@@ -0,0 +1,19 @@
+
+
+help:
+	@echo 'Usage:'
+	@echo '	$$ make [action]'
+	@echo
+	@echo 'Example:'
+	@echo '	$$ make'
+	@echo '	$$ make help'
+	@echo
+	@echo '	$$ make box-build'
+	@echo '	$$ make box-run'
+	@echo
+	@echo '	$$ make master-build'
+	@echo
+	@echo '	$$ make debian-iso-builder-template'
+	@echo '	$$ make debian-iso-builder-respin-xfce'
+	@echo
+.PHONY: help
