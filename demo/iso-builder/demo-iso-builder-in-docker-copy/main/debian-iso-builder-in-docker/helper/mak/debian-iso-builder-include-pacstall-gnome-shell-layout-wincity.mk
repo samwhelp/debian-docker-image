b6850f29c200bf345e89693accec8efb43ad6bd8
samwhelp <samwhelp@users.noreply.github.com>
2026-03-14 04:12:28 +0800

    fix

---
+++ demo/iso-builder/demo-iso-builder-in-docker-copy/main/debian-iso-builder-in-docker/helper/mak/debian-iso-builder-include-pacstall-gnome-shell-layout-wincity.mk
@@ -0,0 +1,5 @@
+
+
+debian-iso-builder-include-pacstall-gnome-shell-layout-wincity:
+	$(REF_PLAN_DIR_PATH)/master-build.sh "debian-iso-builder-include-pacstall-gnome-shell-layout-wincity"
+.PHONY: debian-iso-builder-include-pacstall-gnome-shell-layout-wincity
---
+++ demo/iso-builder/demo-iso-builder-in-docker-start/main/debian-iso-builder-in-docker/helper/mak/debian-iso-builder-include-pacstall-gnome-shell-layout-maccity.mk
@@ -0,0 +1,5 @@
+
+
+debian-iso-builder-include-pacstall-gnome-shell-layout-maccity:
+	$(REF_PLAN_DIR_PATH)/master-build.sh "debian-iso-builder-include-pacstall-gnome-shell-layout-maccity"
+.PHONY: debian-iso-builder-include-pacstall-gnome-shell-layout-maccity
